0d7af02288d1e7f2ddcc830dfeb0a0a2b678d345
thalujyammar <80653094+thalujyammar@users.noreply.github.com>
2022-10-11 08:56:25 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,13 @@
 var vg_1 = "car_imports.vg.json";
 
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_2 = "car_exports.vg.json";
+
+vegaEmbed("#first_vis", vg_1).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#second_vis", vg_2).then(function(result) {
     ("https://vega.github.io/vega/docs/api/view/")
 
 }).catch(console.error);
